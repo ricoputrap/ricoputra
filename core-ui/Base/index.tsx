2867c9ea0e7b3663c6c784bffcd75e56db0d2e8c
Rico Putra Pradana <pradanaricoputra@gmail.com>
2022-03-23 19:14:51 +0700

    implement base component for each page

---
+++ core-ui/Base/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Head from 'next/head'
+
+interface Props {
+  title?: string;
+  children: React.ReactNode;
+}
+
+const Base: React.FC<Props> = ({
+  title = "ricoputra.space",
+  children
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta
+          name="description"
+          content="A frontend web developer specialized in React JS but also using TypeScript, Next JS, styled-components, figma, and docker in the development processes"
+        />
+      </Head>
+
+      <main>{children}</main>
+
+      <footer></footer>
+    </>
+  )
+}
+
+export default Base
