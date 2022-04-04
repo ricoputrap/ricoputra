7396aa8d0979ab6675118d2615d32cf6530aafcb
Rico Putra Pradana <pradanaricoputra@gmail.com>
2022-04-04 21:52:26 +0700

    feat/blogs: init Blogs page without styling

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,12 +1,47 @@
+import { GetStaticProps, NextPage } from 'next'
+import Image from 'next/image'
 import React from 'react'
-import { Heading2 } from '../../core-ui/atoms/Text/index.styles'
+import { HOST } from '../../config'
+import Section from '../../core-ui/atoms/Section'
+import { Heading3 } from '../../core-ui/atoms/Text/index.styles'
+import Base from '../../core-ui/Base'
+import { FlexContainer } from '../../core-ui/Base/index.styles'
+import { Blogs } from '../../types/blog'
 
-const Blog = () => {
+const Blog: NextPage<Blogs> = ({ data }) => {
   return (
-    <div>
-      <Heading2>Blog</Heading2>
-    </div>
+    <Base title='Blogs by ricoputra'>
+      <Section title='Blogs'>
+        <FlexContainer>
+          {data.map(({ id, title, desc, published_at, cover }) => 
+            <div key={id}>
+              <Image 
+                src={cover} 
+                width={365} 
+                height={220} 
+                alt="title"
+              />
+              <p>{published_at}</p>
+              <Heading3>{title}</Heading3>
+              <p>{desc}</p>
+            </div>
+          )}
+        </FlexContainer>
+      </Section>
+    </Base>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const URL: string = HOST + "/api/blogs";
+
+  const req = await fetch(URL);
+  const projectsData = await req.json();
+  const data = projectsData.data;
+
+  return {
+    props: { data }
+  }
 }
 
 export default Blog
